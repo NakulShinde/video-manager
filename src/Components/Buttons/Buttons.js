5fb487db1f4b07416ce5fff91977b459aec51278
NakulShinde <nakulshinde.it@gmail.com>
2018-12-15 15:18:30 +0530

    adding custom button & button scss

--- src/Components/Buttons/Buttons.js
+++ src/Components/Buttons/Buttons.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
 export const CustomButton = (props) => {
-    return <button>
+    return <button
+        disabled={props.isDisable
+        ? true
+        : false}
+        className={props
+        .customClass
+        .join(' ')}
+        onClick=
+        { (e) =>{ e.preventDefault(); props.onClickHandler() } }>
         {props.text}
     </button>
 }
